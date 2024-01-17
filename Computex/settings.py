5b90456e863994d34265ead25e67ef1d39dc89da
Victor <vsifah@gmail.com>
2024-01-17 13:54:17 +0100

    Adding stuff

--- Computex/settings.py
+++ Computex/settings.py
@@ -93,13 +93,9 @@ WSGI_APPLICATION = "Computex.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "computex",
-        "USER": "admin",
-        "PASSWORD": "12345",
-        "HOST": "localhost",
-        "PORT": "5432"
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
