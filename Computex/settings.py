8f9398d0acc32cbca6556c375f871b16234fc64c
Victor Ifah <97197194+GerromeSieger@users.noreply.github.com>
2024-01-18 12:07:25 +0100

    Update settings.py

--- Computex/settings.py
+++ Computex/settings.py
@@ -93,9 +93,9 @@ WSGI_APPLICATION = "Computex.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
