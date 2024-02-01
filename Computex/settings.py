f84922396a3489ceeb7f2e0076e90b7855e9a6ec
Victor Ifah <97197194+GerromeSieger@users.noreply.github.com>
2024-02-01 15:05:20 +0100

    Update settings.py

--- Computex/settings.py
+++ Computex/settings.py
@@ -92,10 +92,20 @@ WSGI_APPLICATION = "Computex.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+#}
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "computex",
+        "USER": "admin",
+        "PASSWORD": "12345",
+        "HOST": "localhost",
+        "PORT": "5432"
     }
 }
 
